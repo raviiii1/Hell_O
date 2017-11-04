093caf8ab9d12ce8e99a522abe41e9379992f003
Ravi Prakash <ravi.prakash@kronos.com>
2017-11-04 15:13:01 +0530

    Mesage modified.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var port  = process.env.PORT || 1337;
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/', function(req, res){
-  res.status(200).send("Hello World!");
+  res.status(200).send("Hello World! How's going?");
 });
 
 app.listen(port, function(){
@@ -17,7 +17,7 @@ app.listen(port, function(){
 app.post('/hello', function(req, res, next){
   var username = req.body.user_name;
   var botPayload = {
-    text : 'Hello' + username + 'I am Hell-O bot!'
+    text : 'Hello, ' + toCamelCase(username.split('.')[0]) + '! I am Hell-O bot!'
   };
   if(username != 'slackbot'){
     return res.status(200).json(botPayload);
@@ -25,3 +25,10 @@ app.post('/hello', function(req, res, next){
     return res.status(200).end();
   }
 });
+
+String.prototype.toCamelCase = function(str) {
+    return str
+        .replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+        .replace(/\s/g, '')
+        .replace(/^(.)/, function($1) { return $1.toLowerCase(); });
+}
